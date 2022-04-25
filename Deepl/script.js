8fee385b18f8c04e76039f949402e8775b35795b
Boris Grunwald <borisgrunwald@gmail.com>
2022-04-25 14:06:31 +0200

    added copyTextToClipboard

--- Deepl/script.js
+++ Deepl/script.js
@@ -35,4 +35,13 @@
 
     });
 
+    function copyTextToClipboard(text) {
+
+        navigator.clipboard.writeText(text).then(function() {
+          console.log('Async: Copying to clipboard was successful!');
+        }, function(err) {
+          console.error('Async: Could not copy text: ', err);
+        });
+      }
+
 })();
